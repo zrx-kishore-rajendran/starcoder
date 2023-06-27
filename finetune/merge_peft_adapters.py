f45dadad77105f7aaef6f7303183f865eda580c1
kishore <kishore.rajendran@zoomrx.com>
2023-06-27 15:55:23 +0530

    added output dir

--- finetune/merge_peft_adapters.py
+++ finetune/merge_peft_adapters.py
@@ -10,6 +10,7 @@ def get_args():
     parser.add_argument("--base_model_name_or_path", type=str, default="bigcode/large-model")
     parser.add_argument("--peft_model_path", type=str, default="/")
     parser.add_argument("--push_to_hub", action="store_true", default=True)
+    parser.add_argument("--output_dir", type=str)
 
     return parser.parse_args()
 
@@ -29,12 +30,12 @@ def main():
 
     if args.push_to_hub:
         print(f"Saving to hub ...")
-        model.push_to_hub(f"{args.base_model_name_or_path}-merged", use_temp_dir=False, private=True)
-        tokenizer.push_to_hub(f"{args.base_model_name_or_path}-merged", use_temp_dir=False, private=True)
+        model.push_to_hub(f"{args.output_dir}-merged", use_temp_dir=False, private=True)
+        tokenizer.push_to_hub(f"{args.output_dir}-merged", use_temp_dir=False, private=True)
     else:
-        model.save_pretrained(f"{args.base_model_name_or_path}-merged")
-        tokenizer.save_pretrained(f"{args.base_model_name_or_path}-merged")
-        print(f"Model saved to {args.base_model_name_or_path}-merged")
+        model.save_pretrained(f"{args.output_dir}-merged")
+        tokenizer.save_pretrained(f"{args.output_dir}-merged")
+        print(f"Model saved to {args.output_dir}-merged")
 
 if __name__ == "__main__" :
     main()
